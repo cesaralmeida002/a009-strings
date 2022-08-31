5828feee681fcd761d96da968b5048f24467ba39
cesaralmeida002 <cesaralmeida002@gmail.com>
2022-08-31 16:43:58 -0300

    Exercicio 31/08/2022

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const usuario = prompt("Qual seu nome?")
+const Comida1 = prompt("Qual sua 1º comida favorita?")
+const Comida2 = prompt("Qual sua 2º comida favorita?")
+const Comida3 = prompt("Qual sua 3º comida favorita?")
+
+console.log(`Estas são as comidas favoritas do usuário são: ${usuario} \n ${Comida1} \n ${Comida2} \n ${Comida3}`)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
-//Crie a const para a frase aqui
+const frase1 = "BOAS VINDAS, mas não deixe o gato sair"
+const frase = (`Jorge tem uma casa verde e com portão azul, com os dizeres:\" ${frase1.toUpperCase()}\"`)
+
+    console.log(frase)
+
+let novaFrase = frase.replace("verde","rosa").replace("azul","laranja") 
+    console.log(novaFrase)
+
+let temCor1 = novaFrase.includes("verde")
+let temcor2 = novaFrase.includes("laranja")
+    console.log(temCor1 , temcor2)
